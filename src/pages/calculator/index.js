911a8da80b2d6b8252300ca742956eb93217f259
Koushik Roy <jc.koushikroy@gmail.com>
2023-07-13 22:31:14 +0530

    initial commit

---
+++ src/pages/calculator/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Calculator() {
+  return (
+    <>
+    <h1>Calculator page</h1>
+    </>
+  )
+}
+
+export default Calculator
---
+++ src/pages/todo-list/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <>
+    <h1>To do list page</h1>
+    </>
+  )
+}
+
+export default Todo
